af92a0cf149d95041e1d60f05f56e47064c5139a
jeniruiz <jenifferuiz@gmail.com>
2024-09-12 08:32:59 -0300

    Create wom.css

---
+++ wom.css
@@ -0,0 +1,77 @@
+body {
+    background-image: url("https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjFsbzd6bWxya3NvM3BtNjBhenBsaWl0NW1vazBzYnB3Z3l1MXZ2diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/D9yDyJt85ljQZ9W8iT/giphy.gif");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+h1 {
+    color:#b1186c;
+    text-align: center;
+    background-color: black;
+    font-family: chiller;
+    font-size:50px;
+    border-left: 5px dashed #45cfb4;
+    border-right: 5px dashed #45cfb4;
+    border-top: 5px dashed #45cfb4;
+    border-bottom: 5px dashed #45cfb4;
+}
+
+p {
+    text-align: center;
+    font-size:30px;
+}
+
+h2 {
+    font-size:30px;
+    text-decoration: line-through;
+    color: #420d2a;
+}
+
+h3 {
+ font-size:30px;
+ text-decoration: line-through;
+ color: #420d2a;
+}
+
+u {
+    background-color: #45cfb4;
+    border-radius:5px;
+    padding: 5px;
+}
+
+footer {
+    text-align: right;
+    background-color: rgb(49, 8, 83);
+    opacity: 0.7;
+    color: white;
+    font-size:30px;
+}
+
+.wom {
+    border: 5px solid white;
+    display: block;
+    margin: auto;
+    width: 500px;
+    height: 500px;
+    }
+
+.wom2 {
+    border: 5px solid white;
+    display: block;
+    margin: auto;
+    width: 500px;
+    height: 500px;
+}
+
+div {
+  background-color: black;
+  color: #b1186c;
+  font-size:30px;
+  font-family: chiller;
+  text-align: center;
+  border-left: 10px dashed #45cfb4;
+  border-right: 10px dashed #45cfb4;
+  border-top: 10px dashed #45cfb4;
+  border-bottom: 10px dashed #45cfb4;
+}
